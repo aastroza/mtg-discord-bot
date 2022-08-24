3349fcfcd3d918cfa3dbb65ed41f395ef9c706c1
Alonso Astroza Tagle <aastroza@ing.uchile.cl>
2022-08-24 19:00:47 -0400

    options chrome

--- generate.py
+++ generate.py
@@ -145,9 +145,9 @@ def _download_card(card):
 def _get_driver():
     options = Options()
     options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
+    #options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+    #options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--window-size=1920,1080')
     #options.add_argument('--force-device-scale-factor=0.75')
     #options.add_argument('--high-dpi-support=0.75')
